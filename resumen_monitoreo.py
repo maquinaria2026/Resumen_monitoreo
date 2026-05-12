d130fd9aa6b60d5b10f0cafde9f6abdbc3d3de2a
maquinaria2026 <smaquinaria@manuelita.com>
2026-05-12 18:30:12 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- resumen_monitoreo.py
+++ resumen_monitoreo.py
@@ -73,10 +73,13 @@ st.markdown("""
 @st.cache_data
 def cargar_datos(archivo):
     df = pd.read_csv(archivo, sep=';', encoding='utf-8')
+    # convertir a date time
     df['Fecha/Hora'] = pd.to_datetime(df['Fecha/Hora'], dayfirst=True, errors='coerce')
+    # Eliminar filas invalidas
     df = df.dropna(subset=['Fecha/Hora'])
     df['Equipo'] = df['Equipo'].astype(str)
-    df['Hora'] = df['Fecha/Hora'].dt.floor('H')
+    #crear columna de hora
+    df['Hora'] = df['Fecha/Hora'].dt.floor('h')
     df = df.sort_values(['Equipo','Fecha/Hora'])
     df['tiempo_seg'] = df.groupby('Equipo')['Fecha/Hora'].diff().shift(-1).dt.total_seconds().fillna(0)
     df.loc[df['Grupo Operacion'] == 'AUXILIAR', 'Grupo Operacion'] = 'PRODUCTIVO'
